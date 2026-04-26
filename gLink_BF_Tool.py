f2b552a2086bb6e67931bc8dee4937b859f8575b
Art3mis_XP <northwalker676@gmail.com>
2026-04-26 00:57:31 -0400

    Update gLink_BF_Tool.py
    Single definition used instead of two to call brute force function

--- gLink_BF_Tool.py
+++ gLink_BF_Tool.py
@@ -7,19 +7,17 @@ base_url = "https://grabify.link/track/"
 
 alphanumeric = list(string.ascii_uppercase) + [str(n) for n in range(10)]
 
-def value_gen():
-    link_val = ""
-    for _ in range(6):
-        link_val += random.choice(alphanumeric)
-        req = f"https://grabify.link/track/{link_val}"
-    print(req);
-
 def brute_force(x):
     print("Potential Grabify Links (Note: Many links are likely false positives): ")
     y = 0
+    link_val = ""
     while y < x:
         y += 1
-        value_gen()
+        for _ in range(6):
+            link_val += random.choice(alphanumeric)
+            req = f"https://grabify.link/track/{link_val}"
+        print(f"{y}. {req}")
+        link_val = ""
 
 Uques = int(input("How many grabify links would you like to try for?: "));
-brute_force(Uques);  
+brute_force(Uques);
